3a6ee32c1e188a0eb9d2f71ddc38b5b47114aba5
guanbo <guanbo2002@gmail.com>
2012-12-14 17:57:03 +0800

    t

--- test/serialport.js
+++ test/serialport.js
@@ -4,6 +4,7 @@ var serialport = require('../lib/serialport');
 
 describe('#Serial Port Unit Test', function(){
 	it('Should success write data to serial port', function() {
+		console.log(json.upload);
 		serialport.write(json.upload)
 	})
 })
